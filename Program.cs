7440b18f1d0338e6aee60388e2b9774cdaa908a6
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-06-07 22:24:58 -0300

    nao terminado

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿
+const double LataTinta = 18, GalaoTinta = 3.6, rend= 3, margem = 0.1;
+
+Console.WriteLine("\n====>ATIVIDADE CASA DE TINTAS<====");
+
+Console.Write("DIGITE A AREA DA SURPERFICIE QUE SERA PINTADA:");
+double area = Convert.ToDouble(Console.ReadLine());
