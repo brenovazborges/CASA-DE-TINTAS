119f774f042a24dc20147f958f06ff5732cdf832
brenovazborges <162759863+brenovazborges@users.noreply.github.com>
2024-06-26 02:41:32 -0300

    ta QUASE pronto

--- Program.cs
+++ Program.cs
@@ -1,7 +1,28 @@
-﻿
-const double LataTinta = 18, GalaoTinta = 3.6, rend= 3, margem = 0.1;
+﻿//é 1:50 da manhã e esse código já fez minha calvície avançar mais 3cm, aproveitem :D
 
-Console.WriteLine("\n====>ATIVIDADE CASA DE TINTAS<====");
+double litrosLata = 18, litrosGalao = 3.6, margem = 1.1;
+double litrosNecessarios;
+int area, rendimento = 3;
 
-Console.Write("DIGITE A AREA DA SURPERFICIE QUE SERA PINTADA:");
-double area = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("==ATIVIDADE CASA DE TINTAS=");
+Console.Write("\nÁrea a ser pintada (m²)...:") ;
+area = Convert.ToInt32(Console.ReadLine());
+litrosNecessarios = (area / rendimento) * margem;
+
+Console.WriteLine($"\nTotal de {litrosNecessarios:F1} litros de tinta necessarios.\n");
+
+int latasNessesarias = (int) (litrosNecessarios / litrosLata);
+double restoLata = (litrosNecessarios % litrosLata);
+int galoesNecessarios = (int)Math.Ceiling(restoLata / litrosGalao);
+
+    
+if (restoLata > 0 && galoesNecessarios * litrosGalao < restoLata)
+{
+    latasNessesarias++;
+    galoesNecessarios = 0;
+}
+
+Console.WriteLine("Distribuição:");
+Console.WriteLine($"Latas...: {latasNessesarias} unidade(s)");
+Console.WriteLine($"Galões..: {galoesNecessarios} unidade(s)");
+
